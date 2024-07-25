db9ad42cc7549a4d54212733c9bff25ddb6d994a
dacha vineeth kumar <vineeth25d60@gmail.com>
2024-07-25 14:43:47 -0500

    addded deploy

--- lambda_function.py
+++ lambda_function.py
@@ -102,7 +102,7 @@ def lambda_handler(event, context):
             workflow_job_name = workflow_job['name']
             workflow_job_status = workflow_job['status']
             
-            if "Analyze" in workflow_job_name and workflow_job_status == "queued":
+            if "deploy" in workflow_job_name and workflow_job_status == "queued":
                 print("CodeQL job found and status is queued")
                 job = "CodeQL"
                 
